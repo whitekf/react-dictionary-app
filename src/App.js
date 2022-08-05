71ef7d76de7a61eebc22bcb19910a4e1a8066207
whitekf <90643199+whitekf@users.noreply.github.com>
2022-08-05 12:10:14 -0400

    Added GitHub repo URL and my portfolio link in the footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ export default function App() {
   return (
     <div className="App">
       <h1>Dictionary App</h1>
+      <p></p>
+      <footer>
+        {" "}
+        This project is coded by{" "}
+        <a href="https://kassiewhite.netlify.app/index.html" target="_blank">
+          Kassie White{" "}
+        </a>{" "}
+        and it's{" "}
+        <a
+          href="https://github.com/whitekf/react-dictionary-app"
+          target="_blank"
+        >
+          Open-Sourced
+        </a>
+      </footer>
     </div>
   );
 }
