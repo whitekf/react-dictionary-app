69aa77f5e2658bf16cbb03c90647fb6b1ce6fdd6
whitekf <90643199+whitekf@users.noreply.github.com>
2022-08-09 16:06:45 -0400

    Added logo

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
-      <button className="btn btn-primary">Click here</button>
-      <Dictionary />
-      <p></p>
-      <footer>
-        {" "}
-        This project is coded by{" "}
-        <a href="https://kassiewhite.netlify.app/index.html" target="_blank">
-          Kassie White{" "}
-        </a>{" "}
-        and it's{" "}
-        <a
-          href="https://github.com/whitekf/react-dictionary-app"
-          target="_blank"
-        >
-          Open-Sourced
-        </a>
-      </footer>
+      <div className="container">
+        <img src="./KassLogoK.jpg" className="AppLogo" alt="logo" />
+        <h1>Dictionary App</h1>
+        <button className="btn btn-primary">Click here</button>
+        <Dictionary />
+        <p></p>
+        <footer>
+          {" "}
+          This project is coded by{" "}
+          <a href="https://kassiewhite.netlify.app/index.html" target="_blank">
+            Kassie White{" "}
+          </a>{" "}
+          and it's{" "}
+          <a
+            href="https://github.com/whitekf/react-dictionary-app"
+            target="_blank"
+          >
+            Open-Sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
