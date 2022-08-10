02aaeb3dfa808a2287c219183150847a18a80f0a
whitekf <90643199+whitekf@users.noreply.github.com>
2022-08-09 23:16:05 -0400

    testing space variable in place of concatenation with " "

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function Meaning(props) {
   console.log(props.meaning);
+  let space = " ";
   return (
     <div className="Meaning">
       <h6>
@@ -10,24 +11,25 @@ export default function Meaning(props) {
       </h6>
       <br />
       <h5>
-        <strong> Synonym(s):</strong> {" " + props.meaning.synonyms + " " + " "}{" "}
+        <strong> Synonym(s):</strong>{" "}
+        {{ space } + props.meaning.synonyms + { space }}{" "}
       </h5>
       <br />
       <strong> Antonym(s): </strong>
-      <h5>{" " + props.meaning.antonyms + " " + " "}</h5>
+      <h5>{{ space } + props.meaning.antonyms + { space }}</h5>
       <br />
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <h5>
               <strong> Definition: </strong>
-              {" " + definition.definition}
+              {{ space } + definition.definition}
             </h5>
 
             <h5>
               {" "}
               <h6>
-                - Example: <em> {" " + definition.example}</em>
+                - Example: <em> {{ space } + definition.example}</em>
               </h6>
               <br />
             </h5>
