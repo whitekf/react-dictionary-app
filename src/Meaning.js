8b26629aee8a4a3b77bbe63b126b423beaa8a851
whitekf <90643199+whitekf@users.noreply.github.com>
2022-08-09 23:17:12 -0400

    removed space attempt for netlify deploy...

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Meaning(props) {
   console.log(props.meaning);
-  let space = " ";
+
   return (
     <div className="Meaning">
       <h6>
@@ -11,25 +11,24 @@ export default function Meaning(props) {
       </h6>
       <br />
       <h5>
-        <strong> Synonym(s):</strong>{" "}
-        {{ space } + props.meaning.synonyms + { space }}{" "}
+        <strong> Synonym(s):</strong> {" " + props.meaning.synonyms + " " + " "}{" "}
       </h5>
       <br />
       <strong> Antonym(s): </strong>
-      <h5>{{ space } + props.meaning.antonyms + { space }}</h5>
+      <h5>{" " + props.meaning.antonyms + " " + " "}</h5>
       <br />
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <h5>
               <strong> Definition: </strong>
-              {{ space } + definition.definition}
+              {" " + definition.definition}
             </h5>
 
             <h5>
               {" "}
               <h6>
-                - Example: <em> {{ space } + definition.example}</em>
+                - Example: <em> {" " + definition.example}</em>
               </h6>
               <br />
             </h5>
