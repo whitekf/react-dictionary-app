04e81afb0119351d761e9ae1cbffc26e9116789d
whitekf <90643199+whitekf@users.noreply.github.com>
2022-08-09 23:38:29 -0400

    Testing removing spaces to see if netlify will deploy site

--- src/Meaning.js
+++ src/Meaning.js
@@ -11,24 +11,24 @@ export default function Meaning(props) {
       </h6>
       <br />
       <h5>
-        <strong> Synonym(s):</strong> {" " + props.meaning.synonyms + " " + " "}{" "}
+        <strong> Synonym(s):</strong> {props.meaning.synonyms}{" "}
       </h5>
       <br />
       <strong> Antonym(s): </strong>
-      <h5>{" " + props.meaning.antonyms + " " + " "}</h5>
+      <h5>{props.meaning.antonyms}</h5>
       <br />
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <h5>
               <strong> Definition: </strong>
-              {" " + definition.definition}
+              {definition.definition}
             </h5>
 
             <h5>
               {" "}
               <h6>
-                - Example: <em> {" " + definition.example}</em>
+                - Example: <em> {definition.example}</em>
               </h6>
               <br />
             </h5>
