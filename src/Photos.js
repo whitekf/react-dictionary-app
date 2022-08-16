8e91621aa8cb56d3397ead458154658ab7e966ce
whitekf <90643199+whitekf@users.noreply.github.com>
2022-08-16 01:42:34 -0400

    Updated alt details for photos to deploy

--- src/Photos.js
+++ src/Photos.js
@@ -16,7 +16,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt="image of the searched term"
+                    alt="searched term"
                   />
                 </a>
               </div>
