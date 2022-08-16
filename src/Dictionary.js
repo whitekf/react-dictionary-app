a52cbf7b40a0dafe1d38809d1b210c5c383590fb
whitekf <90643199+whitekf@users.noreply.github.com>
2022-08-15 22:28:31 -0400

    Added photos to dictionary (via photo api in pexels)

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [term, setTerm] = useState(props.defaultTerm);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleDictionaryResponse(response) {
     setResults(response.data[0]);
@@ -14,6 +16,7 @@ export default function Dictionary(props) {
 
   function handlePexelsResonse(response) {
     console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function search() {
@@ -21,7 +24,7 @@ export default function Dictionary(props) {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${term}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
 
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${term}&per_page=1`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${term}&per_page=80`;
     let pexelsApiKey =
       "563492ad6f91700001000001a57bb7d62efe456788caff1c997a7b2a";
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
@@ -68,6 +71,7 @@ export default function Dictionary(props) {
 
         <div>
           <Results results={results} />
+          <Photos photos={photos} />
         </div>
       </div>
     );
